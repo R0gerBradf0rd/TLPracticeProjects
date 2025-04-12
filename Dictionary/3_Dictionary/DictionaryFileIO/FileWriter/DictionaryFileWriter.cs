172002c42256d3d7cb0c7a44e230d3eb937dba61
SweetAssSlave <3yxpylichka228@gmail.com>
2025-04-12 18:29:03 +0300

    Сделал домашнее задание Dictionary

---
+++ Dictionary/3_Dictionary/DictionaryFileIO/FileWriter/DictionaryFileWriter.cs
@@ -0,0 +1,24 @@
+﻿namespace Dictionary.DictionaryFileIO.FileWriter
+{
+    public class DictionaryFileWriter : IDictionaryWriter
+    {
+        private readonly char _dictionarySeparator;
+
+        public DictionaryFileWriter( char dictionarySeparator )
+        {
+            _dictionarySeparator = dictionarySeparator;
+        }
+
+        public void WriteDictionary( string filePath, Dictionary<string, string> txtDictionary )
+        {
+            using ( StreamWriter writer = new StreamWriter( filePath ) )
+            {
+                foreach ( var word in txtDictionary )
+                {
+                    string line1 = $"{word.Key}{_dictionarySeparator}{word.Value}";
+                    writer.WriteLine( line1 );
+                }
+            }
+        }
+    }
+}
---
+++ Dictionary/3_Dictionary/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main( string[] args )
+        {
+            var dictionaryManager = new DictionaryConsoleApp( "C:\\Users\\Svyat\\Desktop\\Рабочий стол\\Учеба\\TL\\TLPracticeProjects\\Dictionary\\3_Dictionary\\Dictionary.txt", ' ' );
+            dictionaryManager.Start();
+        }
+    }
+}
